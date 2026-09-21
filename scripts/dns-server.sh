c75d24bc0b4a3b1e5fad82ae34cd62b50ea27e39
Dmitriy Kalyuk <kalyukdo@gmail.com>
2026-09-21 19:50:44 +0300

    fix

--- scripts/dns-server.sh
+++ scripts/dns-server.sh
@@ -9,7 +9,6 @@ else
     curl -LO https://github.com/getsops/sops/releases/download/v3.13.3/sops-v3.13.3.linux.amd64
     mv sops-v3.13.3.linux.amd64 /usr/local/bin/sops
     chmod +x /usr/local/bin/sops
-
 fi
 
 echo "=== Импорт PGP ключа для SOPS ==="
