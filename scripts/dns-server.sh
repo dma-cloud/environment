9f42135964ccde018742f427a1ca926738e5a2fd
Dmitriy Kalyuk <kalyukdo@gmail.com>
2026-09-21 19:45:53 +0300

    Add: скрипт инициализации окружения DNS

---
+++ scripts/dns-server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+apt update && apt install -y dnsmasq curl gnupg
+
+if command -v sops &> /dev/null; then
+    echo "=== [ПРОПУСК] SOPS уже установлен в системе: $(sops --version | head -n 1) ==="
+else
+    curl -LO https://github.com/getsops/sops/releases/download/v3.13.3/sops-v3.13.3.linux.amd64
+    mv sops-v3.13.3.linux.amd64 /usr/local/bin/sops
+    chmod +x /usr/local/bin/sops
+
+    echo "=== Импорт PGP ключа для SOPS ==="
+    echo "Вставьте ваш приватный блок ключа (Block) и нажмите Ctrl+D, когда закончите:"
+    echo "------------------------------------------------------------------------"
+
+    if sudo gpg --import; then
+        echo "------------------------------------------------------------------------"
+        echo "🎉 Ключ успешно импортирован в систему напрямую в память!"
+    else
+        echo "❌ Ошибка импорта ключа."
+    fi
+fi
